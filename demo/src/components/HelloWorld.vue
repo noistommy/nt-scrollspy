ef4cd715e77150bf6f64333c3d0a199d09b25f1f
noistommy <hioran@nate.com>
2023-09-28 12:30:54 +0900

    update demo

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup>
-// import Scrollspy from '../../../src';
-import Scrollspy from '../../../dist/nt-scrollspy.es';
-import '../../../dist/nt-scrollspy.css';
+import Scrollspy from '../../../src';
+// import Scrollspy from '../../../dist/nt-scrollspy.es';
+import '../../../src/scrollspy.scss';
 import { ref, onMounted } from 'vue'
 
 defineProps({
@@ -16,7 +16,7 @@ onMounted(() => {
   const smenu = new Scrollspy({
     section: 'section',
     title: '.title',
-    type: 'list', //'list || 'dot' || '{users}'
+    type: 'dot', //'list || 'dot' || '{users}'
     offset: 10
   })
   smenu.init()
@@ -28,27 +28,6 @@ onMounted(() => {
     <section class="s-section">
       <div>
         <h1 class="title">{{ msg }}</h1>
-
-        <div class="card">
-          <button type="button" @click="count++">count is {{ count }}</button>
-          <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test HMR
-          </p>
-        </div>
-
-        <p>
-          Check out
-          <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-            >create-vue</a
-          >, the official Vue + Vite starter
-        </p>
-        <p>
-          Install
-          <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-          in your IDE for a better DX
-        </p>
-        <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
       </div>
     </section>
   </div>
@@ -58,7 +37,7 @@ onMounted(() => {
   <section data-title="section 5"><div class="title">SECTION 5</div></section>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .read-the-docs {
   color: #888;
 }
@@ -77,5 +56,10 @@ section {
 section .title {
   font-size: 2em;
   font-weight: 900;
+}
+.users {
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 </style>
